6870fd599c7a50519e0ae94abb1f04b19dd50481
hanneka <98503316+hanneka@users.noreply.github.com>
2023-02-04 18:13:18 +0800

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { EducationComponent } from './education/education.component';
+import { WorksComponent } from './works/works.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent],
+  declarations: [ AppComponent, HelloComponent, HomeComponent,EducationComponent,WorksComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
